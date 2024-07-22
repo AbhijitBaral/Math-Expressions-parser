cd28570f655d123f57f4536258ec2564bf22c59f
AbhijitBaral <vaishnavo1355@gmail.com>
2024-07-22 13:46:39 +0530

    Objectives update

---
+++ postfixEval.c
@@ -0,0 +1,7 @@
+#include "postfixParser.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+
+
---
+++ postfixEval.h
@@ -0,0 +1,8 @@
+#ifndef POSTFIX_EVAL_H
+#define POSTFIX_EVAL_H
+
+#include "postfixParser.h"
+
+double eval(Stack *postfix);
+
+#endif
